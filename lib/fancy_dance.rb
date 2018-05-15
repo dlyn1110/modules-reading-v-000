d13032255caf06111bbc9b6cc9813c888c3573cf
Dani Lyn <dlyn1110@gmail.com>
2018-05-15 12:45:18 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -7,16 +7,20 @@ module FancyDance
     def jump
       "Look how high I'm jumping!"
     end
- 
+
     def pirouette
       "I'm doing a pirouette"
     end
- 
+
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
 
-
+    module ClassMethods
+      def metadata
+        "this class produces objects that love to dance."
+    end
+end
 
 
   end
